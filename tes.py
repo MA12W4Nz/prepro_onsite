a8fb17e0ba22a0912ee6b8d8ca2e4a7b8e1392b0
MA12W4Nz <109953246+MA12W4Nz@users.noreply.github.com>
2022-07-25 16:35:19 +0700

    Create tes.py

---
+++ tes.py
@@ -0,0 +1,12 @@
+"""123"""
+def main():
+    """asd"""
+    grade1 = float(input())
+    grade2 = 4 - grade1
+    if grade1 >= 2:
+        print("I'm so happy.")
+    elif 1 <= grade1 < 2:
+        print("%.2f" %grade2)
+    else:
+        print("I'm so sad.")
+main()
